5d861bd2446345fae5b3ff82c124259247b2a341
carinasylver <cah.sylver@gmail.com>
2025-07-28 16:44:12 -0300

    animações usando a biblioteca animeJS

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const links = [
   {path: 'atividade_01_css-css3-transitions', title: 'CSS3 Transition'},
   {path: 'atividade_02_css-css3-animation', title: 'CSS3 Animation'},
   {path: 'atividade_03_bibliotecas_frameworks-animate-css', title: 'Animações com a biblioteca Animate CSS'},
+  {path: 'atividade_04_bibliotecas_frameworks-animeJS', title: 'Animações com a biblioteca Anime JS'},
 ]
 
 const listAtividade = document.querySelector('#listaLinks');
